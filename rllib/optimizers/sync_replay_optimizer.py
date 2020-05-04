23c75c6b2e38a34bc7e91b5a2b65905d29bf0948
JeongsooHa <jeongsoo.ha1210@gmail.com>
2020-05-04 14:03:43 +0900

    Added info condition.

--- rllib/optimizers/sync_replay_optimizer.py
+++ rllib/optimizers/sync_replay_optimizer.py
@@ -197,15 +197,16 @@ class SyncReplayOptimizer(PolicyOptimizer):
 
     def input_data_and_check_packetid(self, policy_id, row):
         # Check busy node
-        if row["obs"][-3]:
+        import ipdb; ipdb.set_trace()
+        if not len(row["infos"]):
             return None
         else:
             # obs = ["C", "H", "delay", "delivery", "is_busy", "nACKs", "packet id"]
             # Check delivery flag
-            if row["obs"][-4] == 1:
+            if row["infos"]["delivery"][0] == 1:
                 # Find same packet id
                 for trajectory in self.temp_replay_buffers[policy_id]:
-                    if trajectory["obs"][-1] == row["obs"][-1]:
+                    if trajectory["infos"]["packetid"][0] == row["infos"]["packetid"][0]:
                         # Change rewards
                         trajectory["rewards"] += self.num_agents
                         break
